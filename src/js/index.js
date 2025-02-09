7686ba744fbfb8c73433157319f48b9af4300acc
ASOMSJ <bawazirabdulmaik@gmail.com>
2025-02-09 22:43:13 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min';
+import '../sass/style.scss';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+
+$(function() {
+    
+
+    var pathname = window.location.pathname;//إحضار المسار المتواجدين فيه
+    console.log(pathname);
+    $('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+    
+    //إضافة الصنف الفعال للصفحات المتعلقة بصفحة المدونة
+    if (pathname == "/login.html" || pathname == "/login.html") {
+      $('.navbar-nav > li > a[href="/login.html"]').parent().addClass('active');
+    }
+
+   
+    
+ 
+});
+
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("date").innerHTML = year;
