46d1a63f048230f531c22c2825d68aace8601bd9
ASOMSJ <bawazirabdulmaik@gmail.com>
2025-02-13 22:35:21 +0300

    Home page development is complete.

--- src/js/index.js
+++ src/js/index.js
@@ -6,32 +6,81 @@ import '../sass/style.scss';
 import '@fortawesome/fontawesome-free/js/all.min';
 
 
-$(function() {
-    
+$(function () {
 
-    var pathname = window.location.pathname;//إحضار المسار المتواجدين فيه
-    console.log(pathname);
-    $('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
-    
-    //إضافة الصنف الفعال للصفحات المتعلقة بصفحة المدونة
-    if (pathname == "/login.html" || pathname == "/login.html") {
-      $('.navbar-nav > li > a[href="/login.html"]').parent().addClass('active');
-    }
-
-    //  تغيير خلفية القائمة العلويه عند التمرير أكثر من 50 بكسل
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 50) { 
-          $(".navbar").addClass("scrolled");
-      } else {
-          $(".navbar").removeClass("scrolled");
+  //  تحديد العنصر الذي يتم النقر عليه
+  $(window).on('scroll', function () {
+    var scrollPos = $(document).scrollTop();
+    $('.navbar-nav a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        $('.navbar-nav li').removeClass("active");
+        currLink.parent().addClass("active");
       }
+      else {
+        currLink.parent().removeClass("active");
+      }
+    });
   });
 
-   
-    
- 
+
+  //  تغيير خلفية القائمة العلويه عند التمرير أكثر من 50 بكسل
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $(".navbar").addClass("scrolled");
+    } else {
+      $(".navbar").removeClass("scrolled");
+    }
+  });
+
+
+
+
 });
 
+
+//  تحريك الصفحة بسلاسة عند النقر على الروابط
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+// تحديد السنة الحالية
 var date = new Date();
 var year = date.getFullYear();
 document.getElementById("date").innerHTML = year;
+
+
+
